370d2734daa1269fc4fe5ea60dadf08a76c88483
Kangboom <jkl6147@naver.com>
2024-05-22 17:02:05 +0900

    update project

---
+++ src/main/java/com/example/BigK/BigKApplication.java
@@ -0,0 +1,13 @@
+package com.example.BigK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BigKApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BigKApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/BigK/BigKApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BigK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BigKApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
